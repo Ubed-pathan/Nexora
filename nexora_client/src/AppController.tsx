63da42639f3a114268ee353af8c6e9d2501f5418
Ubed-pathan <ubedpathan818@gmail.com>
2025-04-19 08:24:43 +0530

    project completed

--- nexora_client/src/AppController.tsx
+++ nexora_client/src/AppController.tsx
@@ -63,7 +63,7 @@ export function MainApp() {
     // }
 
     if (loading) {
-        return <div className="flex flex-col justify-center items-center w-full">
+        return <div className="flex justify-center items-center w-full">
         <Loader className="w-10 h-10 text-primary-100 animate-spin" />
       </div>;
     }
